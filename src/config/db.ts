3154ff22d2548b7dbbdd68f64d8cc6f2fe2904aa
juan manuel <jmgv87191@gmail.com>
2025-04-02 10:54:57 -0700

    asd

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,17 @@
 import mongoose from 'mongoose'
 
 export const connectDB = async () => {
+    
 
     try {
+        const url = 'mongodb+srv://jmgv87191:dk2SYJaqNQbTVn3M@cluster0.2tbde.mongodb.net/linktree_node_typescript'
+        const connection = await mongoose.connect(url)
+
+        console.log(connection)
+        console.log(`mongodb conectado`)
         
     } catch (error) {
+        console.log(error)
         
     }
 
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import router from './router';
+import { connectDB } from './config/db'
 
 const app = express();
 
