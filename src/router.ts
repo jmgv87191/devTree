0d2ca9d241bd9de10f8273e852a930040b80d4a5
juan manuel <jmgv87191@gmail.com>
2025-04-09 13:54:52 -0700

    asd

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,17 @@
 import { Router } from 'express'
+import User from './models/User';
 
 const router = Router();
 
 // autenticacion y registro
 
-router.post('/auth/register',( req, res )=>{
+router.post('/auth/register',async( req, res )=>{
 
     console.log(req.body)
+
+    await User.create(req.body)
+
+
 
     res.json({
         msg:"asd"
